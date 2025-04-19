a7e9959b0adfeab8228ad60cf0286cecbeeb8716
barborik <barborikadam@gmail.com>
2025-04-19 23:13:57 +0200

    repo init

---
+++ main.py
@@ -0,0 +1,53 @@
+import io
+
+from PIL import Image
+from flask import Flask, request, make_response
+
+app = Flask(__name__)
+
+pixels = [[0] * 40 for _ in range(40)]
+
+
+@app.route("/", methods=["GET"])
+def home():
+    return "TEST"
+
+
+@app.route("/status/", methods=["GET", "POST"])
+def camera_status():
+    pass
+
+
+@app.route("/last/", methods=["GET", "POST"])
+def last_photo():
+    if request.method == "GET":
+        data = pixels2bmp(pixels)
+        response = make_response(data)
+        response.headers.set("Content-Type", "image/bmp")
+
+        return response
+
+    if request.method == "POST":
+        data = request.data.decode("utf-8")
+        print(data)
+
+
+def pixels2bmp(pixels):
+    width = len(pixels[0])
+    height = len(pixels)
+    flat = []
+
+    for row in pixels:
+        for pixel in row:
+            flat.append(pixel)
+
+    image = Image.frombytes('L', (width, height), bytes(flat))
+
+    bmp_buffer = io.BytesIO()
+    image.save(bmp_buffer, format='BMP')
+
+    return bmp_buffer.getvalue()
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
