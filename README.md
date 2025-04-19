6a3e2a6934d2e51a57f979733f28d606789be1d5
barborik <barborikadam@gmail.com>
2025-04-19 23:43:28 +0200

    fixed urls

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # lpe-monopixel-http
+
+```
+$ python -m venv venv
+$ source venv/bin/activate
+$ pip install -r requirements.txt
+$ python main.py
+```
--- main.py
+++ main.py
@@ -8,17 +8,17 @@ app = Flask(__name__)
 pixels = [[0] * 40 for _ in range(40)]
 
 
-@app.route("/", methods=["GET"])
+@app.route("/lpe/", methods=["GET"])
 def home():
     return "TEST"
 
 
-@app.route("/status/", methods=["GET", "POST"])
+@app.route("/lpe/status/", methods=["GET", "POST"])
 def camera_status():
     pass
 
 
-@app.route("/last/", methods=["GET", "POST"])
+@app.route("/lpe/last/", methods=["GET", "POST"])
 def last_photo():
     if request.method == "GET":
         data = pixels2bmp(pixels)
